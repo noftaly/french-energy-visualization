f88f71296e11e0941b38d95ec6453c85dcd492ba
Elliot Maisl <elliot.maisl@gmail.com>
2023-10-13 19:31:15 +0200

    Add more Production explanations

--- pages/2_⚡️_Production.py
+++ pages/2_⚡️_Production.py
@@ -61,7 +61,10 @@ c_scale = alt.Scale(domain=list([*color_scale_hex.keys(), 'propre', 'sale']), ra
 energy_chart = alt.Chart(energy_data).mark_arc().encode(
     theta='sum(energy_value):Q',
     color=alt.Color('energy_source', scale=c_scale, title='Energy source'),
-    tooltip=[alt.Tooltip('sum(energy_value):Q', title='Energy (MW)')],
+    tooltip=[
+        alt.Tooltip('energy_source', title=" "),
+        alt.Tooltip('sum(energy_value):Q', title='Energy (MW)')
+    ],
 ).properties(
     width=300,
     height=300
@@ -74,7 +77,10 @@ clean_dirty_energy_data['energy_source'] = clean_dirty_energy_data['energy_sourc
 clean_dirty_energy_chart = alt.Chart(clean_dirty_energy_data).mark_arc().encode(
     theta='sum(energy_value):Q',
     color=alt.Color('energy_source', scale=c_scale, title='Energy source'),
-    tooltip=[alt.Tooltip('sum(energy_value):Q', title='Energy (MW)')],
+    tooltip=[
+        alt.Tooltip('energy_source', title=" "),
+        alt.Tooltip('sum(energy_value):Q', title='Energy (MW)'),
+    ],
 ).properties(
     width=300,
     height=300
@@ -82,7 +88,21 @@ clean_dirty_energy_chart = alt.Chart(clean_dirty_energy_data).mark_arc().encode(
 
 st.altair_chart(energy_chart | clean_dirty_energy_chart)
 
-st_graph_title('Use of energy sources per region over a given period')
+st.markdown("""
+            This two pie charts show the distribution of energy sources over a given period in France. The first
+            one shows the distribution of energy sources, and the second one groups them into the "Clean" and "Dirty"
+            categories.
+
+            You might have noticed that the second chart has a third category: "Nuclear". This is because the
+            categorisation of nuclear energy is still pretty ambiguous as it is considered clean and renewable by some,
+            only one of those by others, and neither by a minority.
+
+            What jumps out once again is that the nuclear energy source is by far the most used in France. What might
+            surprise others is that "Clean" energy sources are actually the second most used energy source! It is more
+            or less evenly distributed between solar, hydraulic and bioenergies and far ahead of thermal energy.
+            """)
+
+st_graph_title('Production of energy sources per region over a given period')
 
 energy_sources_heatmap_data = get_energy_sources_data_with_region(df, df_by_day, choice_period, choice_region)
 energy_sources_heatmap_data = energy_sources_heatmap_data.groupby(['libelle_region']).agg({
@@ -94,10 +114,40 @@ energy_sources_heatmap_data = energy_sources_heatmap_data.groupby(['libelle_regi
     'bioenergies': 'sum',
 })
 
-fig = px.imshow(energy_sources_heatmap_data.apply(lambda x: round(x, -4)),
-                text_auto=True,
-                aspect='auto',
-                color_continuous_scale='Blues',
-                labels=dict(x="Region", y="Energy source", color="Energy (MW)")
-                )
-st.plotly_chart(fig)
+tab1, tab2 = st.tabs(['All data', 'Without nuclear'])
+
+with tab1:
+    fig = px.imshow(energy_sources_heatmap_data.apply(lambda x: round(x, -4)),
+                    text_auto=True,
+                    aspect='auto',
+                    color_continuous_scale='balance',
+                    labels=dict(x="Region", y="Energy source", color="Energy (MW)")
+                    )
+    st.plotly_chart(fig)
+with tab2:
+    fig = px.imshow(energy_sources_heatmap_data.drop(columns=['nucleaire']).apply(lambda x: round(x, -4)),
+                    text_auto=True,
+                    aspect='auto',
+                    color_continuous_scale='balance',
+                    labels=dict(x="Region", y="Energy source", color="Energy (MW)")
+                    )
+    st.plotly_chart(fig)
+
+st.markdown("""
+            This heatmap shows the production of energy sources per region. It helps us see what region produces
+            what energy source the most, and compare them easily. Because of the scale of the nuclear, it is hard
+            to see the other energy sources. That's why there is a second tab without the nuclear energy source.
+
+            What we can see, besides the fact that nuclear energy is the most produced, is that we also produce
+            a lot of hydraulic energy, mostly in the Auvergne-Rhône-Alpes region. This is of course due to the alps
+            and all the dams that are built there. The same applies in a much smaller scale for Provences-Alpes-Côte
+            d'Azure, and for Grand-Est with the Jura and for Occitanie with the Pyréenées. We can also see that the
+            Hauts-de-France region produces a lot of wind energy.
+
+            Finally, the Grand-ESt and Hauts-de-France regions produce a lot of thermal energy. This is because
+            there used to be a lot of mines in this region of France.
+
+            Another interesting thing this heatmap shows is which region *doesn't* produce what energy source. For
+            example we can see that although Nuclear Energy is the most produced energy source in France, it is
+            only produced in half of the region.
+            """)
